a628367b4aed39c4a09a8d92a1d91886e03b0f30
joeokivie <josephokivie@hotmail.com>
2024-01-10 13:55:18 -0500

    push code

--- index.js
+++ index.js
@@ -30,25 +30,25 @@ async function viewallemployees() {
 
 
 
-async function init() {
-    const response = await inquirer.prompt(
-        [{
-            type: "list",
-            message: "Welcome to employee tracker please select what you would like to do",
-            choices: ["view all department", "view all job titles", "view all employees",],
-            name: "menu",
+// async function init() {
+//     const response = await inquirer.prompt(
+//         [{
+//             type: "list",
+//             message: "Welcome to employee tracker please select what you would like to do",
+//             choices: ["view all department", "view all job titles", "view all employees",],
+//             name: "menu",
 
-        }]
-    )
-    if (response.menu === "view all department") {
-        viewalldepartment()
+//         }]
+//     )
+//     if (response.menu === "view all department") {
+//         viewalldepartment()
         
-    }
-    if (response.menu === "view all job titles") {
-        viewalljobtitles()
+//     }
+//     if (response.menu === "view all job titles") {
+//         viewalljobtitles()
         
-    }
-}
+//     }
+// }
 
 async function init() {
     const response = await inquirer.prompt(
@@ -75,7 +75,16 @@ async function init() {
     
         
     } 
-
+    const viewAllEmployees = () => {
+        console.log("viewing all employees");
+        // tutor assisted with this const query as I was stuck here
+        const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.dept_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id=role.id LEFT JOIN department on role.department_id=department.id LEFT JOIN employee manager on manager.id=employee.manager_id`;
+        con.query(query, function (err, result) {
+          if (err) throw err;
+          console.table(result);
+          homeMenu();
+        });
+      };
     } 
     
 
