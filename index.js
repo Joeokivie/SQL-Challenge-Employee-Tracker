83f584a3e3f05d73f56f1f947fd0ccf11d157a62
joeokivie <josephokivie@hotmail.com>
2024-01-15 19:57:50 -0500

    resolved conflict

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ async function addallemployees() {
 
 
     )
-    init()
+   
 
     let employee = {
         first_name: response.firstName,
@@ -89,7 +89,8 @@ async function addallemployees() {
     INSERT INTO employees (first_name, last_name, job_id, manager_id)
     VALUES ('${employee.first_name}', '${employee.last_name}', '${employee.job_id}', '${employee.manager_id}')
   `);
-    console.log(employees)
+   
+    init()
 }
 
 async function init() {
@@ -97,7 +98,7 @@ async function init() {
         [{
             type: "list",
             message: "Welcome to employee tracker please select what you would like to do",
-            choices: ["hi", "view all department", "view all job titles", "view all employees", "add department", "add all employees"],
+            choices: ["hi", "view all department", "view all job titles", "view all employees", "add department", "add all employees", "update employee role"],
             name: "menu",
 
         }]
@@ -119,9 +120,11 @@ async function init() {
         addallemployees();
 
     }
+    else if (response.menu === "update employee role") {
+        updateemployees();
+    }
 
-
-
+    
 
 
 
@@ -129,67 +132,23 @@ async function init() {
 
 
 }
-// updates existing employee role
-const updateEmployeeRole= () => {
-    let sql = `SELECT employee.id, employee.first_name, employee.last_name, role.id AS "role_id" FROM employee, role
-    WHERE role.id = employee.role_id`;
-    connection.query(sql, (error, response)=> {
-        if (error) throw error; 
-        let namesArray = [];
-        response.forEach((employee)=> {namesArray.push(`${employee.first_name} ${employee.last_name}`)});
-        //console.log(namesArray);
-        const employees1 = response.map(({id, first_name, last_name})=> ({name: first_name + " " + last_name, value: id}));
-       // console.log(employees1);
-        let sql = `SELECT role.id, role.title FROM role`;
-        
-        connection.query(sql, (error, response)=> {
-            if (error) throw error;
-            let jobs = [];
-            response.forEach((role)=> {jobs.push(role.title)});
-            //console.log(jobs);
-            const jobs1 = response.map(({id, title})=> ({name: title, value: id}));
-            inquirer.prompt([
-                {
-                    type: "list", 
-                    name: "updateEmp", 
-                    message: "Which employee's role are you updating?", 
-                    choices: namesArray
-                }, 
-                {
-                    type: 'list',
-                    name: 'newRole', 
-                    message: 'What is their new role?', 
-                    choices: jobs
-                }
-            ])
-             .then((answer)=> {
-                let currentEmployee = answer.updateEmp;
-                let updatedRole = answer.newRole;
-                let empId;
-                let newRoleId;
-                employees1.forEach((employee)=>{
-                    if(currentEmployee === employee.name) {
-                         empId = employee.value;
+      async function updateemployees() {
+        const response = await inquirer.prompt(
+            [{
+                type: "input",
+                message: "which employee is changing roles",
+                name: "employee",
 
-                    }
-                });
-                jobs1.forEach((role)=>{
-                    if(updatedRole === role.name) {
-                         newRoleId = role.value;
-                    }
-                });
-
-                let sql = `UPDATE employee  SET role_id = ${newRoleId} WHERE employee.id =  ${empId}`;
-                    connection.query(sql, (error, response) => {
-                        if (error) throw error;
-                        viewAllEmployees();
-                        promptUser();
-                    });
-             }
-            )
-        })
-    })
-}
+    
+            },{
+                type: "input",
+                message: "what is the new role",
+                name: "role",
+            }]
+        )
+        const employees = await db.promise().query(`update employees set job_id = ? where employee_id = ? `,[response.role,response.employee])
+        init()
+      }
 
 
 init()
